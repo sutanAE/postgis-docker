4973b05d3f43bc7fe34f97b564a21eb3115ad6b0
sutan mufti <sutan@ambersideenergy.com>
2022-11-29 13:41:42 +0000

    new entrypoint, postgres as CMD

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash 
 
 
-pg_ctlcluster 12 main start
-
-su - postgres -c "psql -c 'create extension postgis;'"
+pg_ctlcluster 12 main start && su - postgres -c "psql -c 'create extension postgis;'" && pg_ctlcluster 12 main stop
 
 
-su - postgres -c "psql"
+# su - postgres -c "psql"
+# pg_ctlcluster 12 main start
+# postgres
+su - postgres -c "PATH=$PATH:/usr/lib/postgresql/12/bin && export PGDATA=/etc/postgresql/12/main && postgres" 
